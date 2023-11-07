e5a57f0e1de19ed8c80696fbaec1923b58c3517f
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-11-07 21:25:52 +0100

    Commit for pulling

--- CLI/Model/Student.cs
+++ CLI/Model/Student.cs
@@ -6,6 +6,7 @@ namespace StudentskaSluzba.Model;
 enum Status{B, S}
 class Student : ISerializable
 {
+    public string id { get; set; }
     public string Last_Name { get; set; }
     public string First_Name { get; set; }
 
