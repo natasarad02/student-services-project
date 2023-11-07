f0cf7ca7bd0eb46190e672ba6795b64bde31ccb4
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-11-07 16:46:15 +0100

    Adding ToCSV method to Address

---
+++ CLI/Model/Address.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace StudentskaSluzba.Model;
+using StudentskaSluzba.Serialization;
+
+class Address : ISerializable
+{
+    /*
+     *  ulica
+     *  broj
+     *  grad
+     *  drzava
+     */ 
+
+    public string Street { get; set; }
+    public int Number { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+
+    public Address(string street, int number, string city, string country)
+    {
+        Street = street;
+        Number = number;
+        City = city;
+        Country = country;
+
+
+    }
+
+    public string[] ToCSV()
+    {
+        string[] csvValues =
+        {
+            Street, Number.ToString(), City, Country
+        };
+        return csvValues;
+    }
+}
