3925286b4767119270206674ed43c2b7386a23ad
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-11-07 19:49:03 +0100

    Adding fields and constructor to EntityDAO

--- CLI/DAO/EntityDAO.cs
+++ CLI/DAO/EntityDAO.cs
@@ -4,3 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 namespace StudentskaSluzba.DAO;
+using StudentskaSluzba.Storage;
+using StudentskaSluzba.Serialization;
+public class EntityDAO<T> where T : ISerializable, new()
+{
+    private readonly List<T> entities;
+    private readonly Storage<T> storage;
+
+    public EntityDAO(string filename)
+    {
+        storage = new Storage<T>(filename + ".txt");
+        entities = storage.Load();
+    }
+
+
+
+}
