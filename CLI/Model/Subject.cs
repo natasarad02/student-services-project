107d6747d53b9b0f91379621d613961454f01f2b
Teo <becejacteodora@gmail.com>
2023-11-07 17:37:59 +0100

    Expansion of Subjects class

--- CLI/Model/Subject.cs
+++ CLI/Model/Subject.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using StudentskaSluzba.Serialization;
 
 namespace StudentskaSluzba.Model;
 
+enum semester {winter, summer}
 class Subject : ISerializable
 {
     public int Id { get; set; }
@@ -10,25 +12,55 @@ class Subject : ISerializable
     public int Espb { get; set; }
 
     // Id professora se serijalizuje
-    public int ProfessorId { get; set; }
+    //public int ProfessorId { get; set; }
 
     // Professor se ne serijalizuje
     public Professor Professor { get; set; }
 
-    public List<Student> Students { get; set; }
+    public List<Student> Students_passed { get; set; }
+
+    public List<Student> Students_attending { get; set; }
+
+    public semester semester { get; set; }
+
+    public int year {
+        get
+        {
+            return year;
+
+        }
+        set
+        {
+
+            if (year < 1 && year > 4) //could be 6 for students in the medical field
+            {
+                year = value;
+            }
+            else
+            {
+                Console.WriteLine("Year must be between 1 and 4!");
+            }
+
+        }
+    }
 
     public Subject()
     {
-        Students = new List<Student>();
+        Students_passed = new List<Student>();
+        Students_attending = new List<Student>();
     }
 
-    public Subject(int id, string name, int espb, int professorId)
+    public Subject(int id, string name, int espb, semester SEM, int school_year, Professor P)
     {
         Id = id;
         Name = name;
         Espb = espb;
-        ProfessorId = professorId;
-        Students = new List<Student>();
+        semester = SEM;
+        Professor = P;
+        //ProfessorId = professorId;
+        year = school_year;
+        Students_passed = new List<Student>();
+        Students_attending = new List<Student>();
     }
 
     public string[] ToCSV()
@@ -38,7 +70,10 @@ class Subject : ISerializable
             Id.ToString(),
             Name,
             Espb.ToString(),
-            ProfessorId.ToString()
+            //ProfessorId.ToString()
+            semester.ToString(),
+            Professor.ToString(),
+            year.ToString()
         };
         return csvValues;
     }
@@ -48,7 +83,11 @@ class Subject : ISerializable
         Id = int.Parse(values[0]);
         Name = values[1];
         Espb = int.Parse(values[2]);
-        ProfessorId = int.Parse(values[3]);
+        //ProfessorId = int.Parse(values[3]);
+        ///Professor = FromCSV(values[3]),
+        //semester = semester.Parse(values[4]),
+        year = int.Parse(values[5]);
+
     }
 
     public override string ToString()
@@ -58,12 +97,8 @@ class Subject : ISerializable
         sb.Append($"NAME: {Name}, ");
         sb.Append($"ESPB: {Espb}, ");
         sb.Append("STUDENTS: ");
-        sb.AppendJoin(", ", Students.Select(student => student.Name));
-        // ili
-        // foreach (Student st in Students)
-        // {
-        //     sb.Append($"{st.Name}, ");
-        // }
+        sb.AppendJoin(", ", Students_passed.Select(student => student.Name));
+        sb.AppendJoin(", ", Students_attending.Select(student => student.Name));
         return sb.ToString();
     }
 }
