4fdb99a1676467bcb700fe4d6e89b0719ae485c8
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-11-07 16:12:28 +0100

    Creating Department class with fields

---
+++ CLI/Model/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using StudentskaSluzba.Model;
+using StudentskaSluzba.Serialization;
+
+namespace StudentskaSluzba.Katedra;
+
+class Department : ISerializable
+{
+    /*
+     * sifra katedre
+     * naziv
+     * sef
+     * spisak
+     */
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Hod { get; set; } // Head Of Department
+
+    public List<Professor> department_professors { get; set; }
+
+
+}
