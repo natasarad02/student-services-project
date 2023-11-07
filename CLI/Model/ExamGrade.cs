34e797397ae2bd52a9829e982282e02ea75ee4ef
Teo <becejacteodora@gmail.com>
2023-11-07 17:06:54 +0100

    Expansion of Exam Grade class

--- CLI/Model/ExamGrade.cs
+++ CLI/Model/ExamGrade.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentskaSluzba.Serialization;
 
-namespace CLI.Model
+namespace CLI.Model;
+
+internal class ExamGrade : ISerializable
 {
-    internal class ExamGrade
+
+    public DateOnly grading_day { get; set; }
+
+    public int grade 
     {
+
+        get 
+        {
+            return grade;
+        
+        }
+
+
+        set {
+
+            if (grade < 11 && grade > 7)
+            {
+                grade = value;
+            }
+            else {
+                Console.WriteLine("Grade must be between 6 and 10!");
+            }
+
+        }
+    
     }
+
+    //student?
+    // predmet
+
+    public string[] ToCSV()
+    {
+        string[] csvValues =
+        {
+            grading_day.ToString(),
+            grade.ToString()
+        };
+        return csvValues;
+    }
+
+    public void FromCSV(string[] values)
+    {
+        grading_day = DateOnly.Parse(values[0]);
+        grade = int.Parse(values[1]);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Date of grading: " + grading_day + ", ");
+        sb.Append("Grade: " + grade);
+
+        return sb.ToString();
+    }
+
+
 }
