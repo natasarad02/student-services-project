f0c7fd85f675aa8b70eb5dae13fb60f7c2070f45
Teo <becejacteodora@gmail.com>
2023-11-07 16:29:43 +0100

    Addition of Index and Exam Grades

---
+++ CLI/Model/Index.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;ial
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using StudentskaSluzba.Model;
+using StudentskaSluzba.Serialization;
+
+namespace CLI.Model;
+
+internal class Index : ISerializable
+{
+
+    public string college_major { get; set; }
+
+    public int number_mark { get; set; }
+
+    public int YOE { get; set; } //year of enrollment
+
+    //dodati ogranicenja za ova 3
+
+    public string[] ToCSV() {
+        string[] csvValues =
+        {
+            college_major.ToString(),
+            number_mark.ToString(),
+            YOE.ToString()
+        };
+        return csvValues;
+    }
+
+    public void FromCSV(string[] values) {
+        college_major = values[0]; 
+        number_mark = int.Parse(values[1]);
+        YOE = int.Parse(values[2]);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("College major mark: " + college_major + ", ");
+        sb.Append("Enrollment number mark: " + number_mark + ", ");
+        sb.Append("Enrollment year: " + YOE);
+
+        return sb.ToString();
+    }
+
+}
