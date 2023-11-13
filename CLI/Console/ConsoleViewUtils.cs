f3d9d5bb0136f2862036c392fda9fe476d85316a
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-11-13 12:10:25 +0100

    Configured SadeInputInt

---
+++ CLI/Console/ConsoleViewUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentskaSluzba.Console;
+
+static class ConsoleViewUtils
+{
+    public static int SafeInputInt()
+    {
+        int input;
+        string raw = System.Console.ReadLine() ?? string.Empty;
+
+        while(!int.TryParse(raw, out input))
+        {
+            System.Console.WriteLine("Not a valid number, try again: ");
+            raw = System.Console.ReadLine();
+        }
+
+        return input;
+    }
+}
