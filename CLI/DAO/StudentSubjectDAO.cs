4365cb2649569d56ecfedd3c313a9d644b3a5de7
Teo <becejacteodora@gmail.com>
2023-11-14 13:14:08 +0100

    Implemented StudentSubject

---
+++ CLI/DAO/StudentSubjectDAO.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentskaSluzba.Storage;
+using StudentskaSluzba.Serialization;
+using StudentskaSluzba.Model;
+
+namespace StudentskaSluzba.DAO
+{
+    class StudentsSubjectsDAO
+    {
+        private readonly List<StudentsSubjects> StudentsSubjectss;
+        private readonly Storage<StudentsSubjects> storage;
+        private StudentDAO studentDAO = new StudentDAO();
+        private SubjectDAO subjectDAO = new SubjectDAO();
+
+        public StudentsSubjectsDAO()
+        {
+            storage = new Storage<StudentsSubjects>("StudentsSubjects" + ".txt");
+            StudentsSubjectss = storage.Load();
+        }
+
+        public StudentsSubjects AddStudentsSubjects(StudentsSubjects StudentsSubjects)
+        {
+            StudentsSubjectss.Add(StudentsSubjects);
+            storage.Save(StudentsSubjectss);
+            return StudentsSubjects;
+        }
+
+        public StudentsSubjects? RemoveStudentsSubjects(int id_s, int id_sub)
+        {
+            StudentsSubjects? oldStudentsSubjects = StudentsSubjectss.Find(s=>s.studentID==id_s && s.subjectID==id_sub);
+            if (oldStudentsSubjects is null) return null;
+
+            StudentsSubjectss.Remove(oldStudentsSubjects);
+            storage.Save(StudentsSubjectss);
+            return oldStudentsSubjects;
+        }
+
+        public List<Student> GetStudents(int id) //find all students that are taking this subject/class
+        {
+
+            List<int> studentIDs = new List<int>();
+
+            List<StudentsSubjects> temp = StudentsSubjectss.FindAll(s => s.subjectID == id);
+
+            foreach(StudentsSubjects s in temp)
+            {
+                studentIDs.Add(s.studentID);
+            }
+
+            List<Student> students = new List<Student>();
+
+            foreach (int studentID in studentIDs)
+            {
+                students.Add(studentDAO.getStudentbyId(studentID));
+            }
+
+            return students;
+
+        }
+
+        public List<Subject> GetSubjects(int studentId)
+        {
+            List<int> subjectIds = new List<int>();
+
+            List<StudentsSubjects> temp = StudentsSubjectss.FindAll(s => s.studentID == studentId);
+
+            foreach (StudentsSubjects s in temp)
+            {
+                subjectIds.Add(s.subjectID);
+            }
+
+            List<Subject> subjects = new List<Subject>();
+
+            foreach (int subjectId in subjectIds)
+            {
+                subjects.Add(subjectDAO.GetSubjectById(subjectId));
+            }
+
+            return subjects;
+        }
+
+
+        public List<StudentsSubjects> GetAllStudentsSubjectss()
+        {
+            return StudentsSubjectss;
+        }
+    }
+}
+
+/*
+
+            
+            
+            List<Student> students = new List<Student>();
+
+            foreach(int studentID in studentIDs)
+            {
+                students.Add(studentDAO.getStudentbyId(studentID));
+            }
+
+            return students;*/
---
+++ CLI/Model/StudentsSubjects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using StudentskaSluzba.Serialization;
+
+namespace StudentskaSluzba.Model;
+
+
+public class StudentsSubjects : ISerializable
+{
+    public int studentID { get; set; }
+
+    public int subjectID { get; set; }
+
+    public string[] ToCSV() {
+
+        string[] csvValues = {
+            studentID.ToString(),
+            subjectID.ToString()
+        };
+        return csvValues;        
+    }
+
+    public void FromCSV(string[] values)
+    {
+        studentID = int.Parse(values[0]);
+        subjectID = int.Parse(values[1]);   
+
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Student id:" +studentID +", ");
+        sb.Append("Subject id" + subjectID);
+
+        return sb.ToString();
+    }
+
+
+}
