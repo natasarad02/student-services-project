bff8975d29e86a87e9c539d209a6137124b9c07b
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-12-15 11:17:10 +0100

    Added ProfessorDTO

--- GUI/DTO/ProfessorDTO.cs
+++ GUI/DTO/ProfessorDTO.cs
@@ -1,1 +1,167 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StudentskaSluzba.Model;
+using System.Windows.Data;
+
+namespace GUI.DTO
+{
+    public class ProfessorDTO : INotifyPropertyChanged
+    {
+        private int num;
+        public int Num
+        {
+            get { return num; }
+            set
+            {
+                if (value != num)
+                {
+                    num = value;
+                    OnPropertyChanged("Num");
+                }
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (value != surname)
+                {
+                    surname = value;
+                    OnPropertyChanged("Surname");
+                }
+            }
+        }
+
+        private Address address;
+        public Address Address
+        {
+            get { return address; }
+            set
+            {
+                if (value != address)
+                {
+                    address = value;
+                    OnPropertyChanged("Address");
+                }
+            }
+        }
+
+        private DateOnly birth_Date;
+        public DateOnly Birth_Date
+        {
+            get { return birth_Date; }
+            set
+            {
+                if (value != birth_Date)
+                {
+                    birth_Date = value;
+                    OnPropertyChanged("Birth_Date");
+                }
+            }
+        }
+
+        private string phone_Number;
+        public string Phone_Number
+        {
+            get { return phone_Number; }
+            set
+            {
+                if (value != phone_Number)
+                {
+                    phone_Number = value;
+                    OnPropertyChanged("Phone_Number");
+                }
+            }
+        }
+
+        private int employment_Year;
+        public int Employment_Year
+        {
+            get { return employment_Year; }
+            set
+            {
+                if (value != employment_Year)
+                {
+                    employment_Year = value;
+                    OnPropertyChanged("Employment_Year");
+                    OnPropertyChanged("Work_Year");
+                }
+            }
+        }
+
+        public int Work_Year => DateTime.Now.Year - Employment_Year;
+
+        private string email_Address;
+        public string Email_Address
+        {
+            get { return email_Address; }
+            set
+            {
+                if (value != email_Address)
+                {
+                    email_Address = value;
+                    OnPropertyChanged("Email_Address");
+                }
+            }
+        }
+
+        private string calling;
+        public string Calling
+        {
+            get { return calling; }
+            set
+            {
+                if (value != calling)
+                {
+                    calling = value;
+                    OnPropertyChanged("Calling");
+                }
+            }
+        }
+
+        public List<Subject> Subjects { get; set; }
+
+        public ProfessorDTO(int card, string name, string surname, Address address1, string phone, DateOnly br_date, int year, string email, string calling)
+        {
+            Num = card;
+            Name = name;
+            Surname = surname;
+            Address = address1;
+            Birth_Date = br_date;
+            Phone_Number = phone;
+            Email_Address = email;
+            Employment_Year = year;
+            Calling = calling;
+            Subjects = new List<Subject>();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
