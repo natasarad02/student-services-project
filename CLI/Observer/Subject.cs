9167eef536d0002e5eb3e5acb5ea2b1f84b0c347
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-12-15 10:14:07 +0100

    Configured Subject and IObserver

--- CLI/Observer/Subject.cs
+++ CLI/Observer/Subject.cs
@@ -4,5 +4,29 @@ namespace CLI.Observer
 {
     internal class Subject
     {
+        private List<IObserver> observers;
+
+        public Subject()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(var observer in observers)
+            {
+                observer.Update();
+            }
+        }
     }
 }
