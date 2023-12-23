4be52c2fba1d9759880ad326b7e9cfae0f9dc93a
Natasa Radmilovic <nata.radmilovic@gmail.com>
2023-12-23 14:19:21 +0100

    Commiting for pulling

---
+++ GUI/View/professor/UpdateProfessor.xaml.cs
@@ -0,0 +1,41 @@
+﻿using GUI.DTO;
+using StudentskaSluzba.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace GUI.View.Professor
+{
+    public partial class UpdateProfessor : MainWindow, INotifyPropertyChanged
+    {
+        public ProfessorDTO Professor { get; set; }
+        private ProfessorDAO professorsDAO { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public UpdateProfessor(ProfessorDAO professorsDAO)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Professor = new ProfessorDTO();
+            this.professorsDAO = professorsDAO;
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            professorsDAO.UpdateProfessor(Professor.ToProfessor());
+            Close();
+        }
+    }
+
+}
---
+++ GUI/View/subject/UpdateSubject.xaml.cs
@@ -0,0 +1,41 @@
+﻿using GUI.DTO;
+using StudentskaSluzba.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace GUI.View.Subject
+{
+    public partial class UpdateSubject : MainWindow, INotifyPropertyChanged
+    {
+        public SubjectDTO Subject { get; set; }
+        private SubjectDAO subjectsDAO { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public UpdateSubject(SubjectDAO subjectsDAO)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Subject = new SubjectDTO();
+            this.subjectsDAO = subjectsDAO;
+
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            subjectsDAO.UpdateSubject(Subject.ToSubject());
+            Close();
+        }
+    }
+
+}
